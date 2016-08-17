7f5550ab8a06d8b9e5602074ae26636eea8ed458
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2016-08-17 17:36:04 +0300

    Update components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,28 @@
 import React, {PropTypes} from 'react';
 import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
 
-const style = {
-  height: 200,
+const paperStyle = {
   width: 300,
   textAlign: 'center',
   position: 'absolute',
-  top: '30%',
-  left: '38%'
+  top: '50%',
+  left: '50%',
+  padding: 10,
+  transform: 'translate(-50%, -50%)'
 };
+const overflow = {
+  overflow: 'hidden'
+}
 
 const Login = ({authenticate}) => (
-  <div className="bg-photo">
-    <Paper style={style} zDepth={2}>
-      <button onClick={authenticate}>Auth</button>
+  <div style={overflow}>
+    <div className="bg-photo"></div>
+    <Paper style={paperStyle} zDepth={5}>
+      <i className="material-icons md-light no-account-icon">account_circle</i>
+      <h1>Welcome Phitartist!</h1>
+      <h2>You're lazy, this is why you are here. Use the lazy login: </h2>
+      <img src="http://i.stack.imgur.com/XzoRm.png" onClick={authenticate} className="google-login-button" alt="Google login"/>
     </Paper>
   </div>
 );
